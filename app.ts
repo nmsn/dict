9fb839b7146a20b24f4bfcce0b86771efabcd165
nmsn <136696700@qq.com>
2021-08-23 15:27:42 +0800

    增加响应中间件

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import * as bodyParser from "koa-bodyparser";
 import { createConnection } from "typeorm";
 import * as cors from "@koa/cors";
 import router from "./src/router";
+import uniteResponse from './src/middleware/uniteResponse';
 
 const app = new koa();
 
@@ -10,8 +11,10 @@ app.use(cors());
 app.use(async (ctx, next) => {
   console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
   await next();
+  console.log(ctx);
 });
 app.use(bodyParser());
+app.use(uniteResponse());
 app.use(router.routes());
 
 
---
+++ src/middleware/uniteResponse.ts
@@ -0,0 +1,47 @@
+interface OptionProps {
+  type?: string;
+  successCode?: number;
+  successMsg?: string;
+  failCode?: number;
+  failMsg?: string;
+};
+
+
+/**
+ * Created by ux34 on 21/6/4
+ * 统一接口返回数据格式
+ * @param option 默认配置
+ * @param option.type 返回的格式
+ * @param option.successCode 请求成功返回的状态码
+ * @param option.successMsg 请求成功返回的消息
+ * @param option.failCode 请求失败返回的状态码
+ * @param option.failMsg 请求失败返回的消息
+ * @returns {function} koa中间件
+ */
+const uniteResponse = (option: OptionProps = {}) => {
+  return async function (ctx, next) {
+    ctx.success = function (data, msg, code) {
+      ctx.status = code || option.successCode || 200;
+      ctx.type = option.type || "json";
+      ctx.body = {
+        code: code || option.successCode || 200,
+        msg: msg || option.successMsg || "success",
+        data: data,
+      };
+    };
+
+    ctx.fail = function (msg, code) {
+      console.log(msg);
+      ctx.status = code || option.failCode || 400;
+      ctx.type = option.type || "json";
+      ctx.body = {
+        code: code || option.failCode || 400,
+        msg: msg || option.failMsg || "fail",
+      };
+    };
+
+    await next();
+  };
+};
+
+export default uniteResponse;
